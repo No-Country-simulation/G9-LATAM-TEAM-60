9f917a98b889fe27510331ebdaefe62577a122bb
SonJorell <jorell34@gmail.com>
2026-08-07 00:11:31 -0400

    chore(frontend): remover archivos no pertenecientes a la capa frontend (backend/recursos)

--- src/main/java/energiai/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package energiai.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/resources/db/migration/V1__create_table_users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id bigint not null GENERATED ALWAYS AS IDENTITY,
-    nombre_completo varchar(150) not null,
-    username varchar(100) not null,
-    password varchar(200) not null UNIQUE,
-
-    primary key(id)
-)
